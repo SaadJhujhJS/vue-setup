d5ab693976482ed5219597cfc76b940fa9faf776
SaadJhujhJS <saadjhujh8@gmail.com>
2025-05-06 13:17:42 +0500

    props,states,events, data-binding done

---
+++ src/components/Forms.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>{{ this.name }}</h1>
+        <h2>{{ this.user }}</h2>
+        <div>
+            <form @submit.prevent="register()">
+                <div>
+                    <input type="text" v-model="user.name">
+                </div>
+                <div>
+                    <input type="submit" value="submit">
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+<script>
+export default{
+    name:"Forms",
+    props:{
+        name:{type:String, required:true}
+    },
+    data:function(){
+        return{
+           user:{
+            name:""
+           }
+        }
+    },
+    methods:{
+        register:function(){
+            alert(this.user.name)
+        }
+    }
+}
+</script>
