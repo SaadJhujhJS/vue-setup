8194fe82a916be6f149c80625efa1c03494c50a3
SaadJhujhJS <saadjhujh8@gmail.com>
2025-06-19 20:00:04 +0500

    fixes

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import CreatedBeforeCreated from '@/components/CreatedBeforeCreated.vue'
 import Forms from '@/components/Forms.vue'
 import Looping from '@/components/Looping.vue'
 import MountedUnmounted from '@/components/MountedUnmounted.vue'
+import UnmountBeforeUnmount from '@/components/UnmountBeforeUnmount.vue'
 import UpdateBeforeUpdate from '@/components/UpdateBeforeUpdate.vue'
 import UserDetails from '@/components/UserDetails.vue'
 import { createRouter, createWebHistory } from 'vue-router'
@@ -14,6 +15,7 @@ const routes = [
   { path: '/MountedUnmounted', name: 'mounted-unmounted', component: MountedUnmounted },
   { path: '/CreatedBeforeCreated', name: 'created-beforecreated', component: CreatedBeforeCreated },
   { path: '/UpdateBeforeUpdate', name: 'update-beforeupdate', component: UpdateBeforeUpdate },
+  { path: '/Unmounted', name: 'Unmounted', component: UnmountBeforeUnmount },
 ]
 
 const router = createRouter({
